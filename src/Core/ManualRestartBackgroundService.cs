790cfc20a0d9c26cebce32ef3e7d8fc5f26ddd5c
Iurii Aksenov <iurii.aksenov@yandex.ru>
2021-04-06 23:23:36 +0700

    [Update] Tokens creator

--- src/Core/ManualRestartBackgroundService.cs
+++ src/Core/ManualRestartBackgroundService.cs
@@ -7,22 +7,23 @@ using Microsoft.Extensions.Logging;
 namespace Core
 {
     /// <summary>
-    /// Base class for implementing a long running <see cref="T:Microsoft.Extensions.Hosting.IHostedService" /> with restart ability.
+    ///     Base class for implementing a long running <see cref="T:Microsoft.Extensions.Hosting.IHostedService" /> with
+    ///     restart ability.
     /// </summary>
     public abstract class ManualRestartBackgroundService : BackgroundService
     {
         private readonly ILogger<ManualRestartBackgroundService> _logger;
-        private CancellationTokenSource _cancelTokenSource = new();
+        private CancellationTokenSource _cancelTokenSource;
         private CancellationToken _token;
 
         protected ManualRestartBackgroundService(ILogger<ManualRestartBackgroundService> logger)
         {
             _logger = logger;
-            _token = _cancelTokenSource.Token;
+            (_cancelTokenSource, _token) = CreateToken();
         }
 
         /// <summary>
-        /// Cancel pending execution
+        ///     Cancel pending execution
         /// </summary>
         /// <returns>True - background worker restarted / False - in another case</returns>
         public bool TryReleaseDelay()
@@ -40,8 +41,8 @@ namespace Core
         }
 
         /// <summary>
-        /// Creates a cancellable task that completes after a specifiendnumber of milliseconds.
-        /// Task can be canceled in any time by calling <see cref="TryReleaseDelay()"/>
+        ///     Creates a cancellable task that completes after a specifiendnumber of milliseconds.
+        ///     Task can be canceled in any time by calling <see cref="TryReleaseDelay()" />
         /// </summary>
         /// <param name="delay">The time span to wait before completing the return task</param>
         /// <returns>A task that represents the time delay.</returns>
@@ -51,23 +52,26 @@ namespace Core
         }
 
         /// <summary>
-        /// Creates a cancellable task that completes after a specifiendnumber of milliseconds.
-        /// Task can be canceled in any time by calling <see cref="TryReleaseDelay()"/>
+        ///     Creates a cancellable task that completes after a specifiendnumber of milliseconds.
+        ///     Task can be canceled in any time by calling <see cref="TryReleaseDelay()" />
         /// </summary>
         /// <param name="delay">The time span to wait before completing the return task</param>
         /// <param name="cancellationToken">A cancellation token to observe while waiting for the task to complete.</param>
         /// <returns>A task that represents the time delay.</returns>
         protected async Task ManualDelay(TimeSpan delay, CancellationToken cancellationToken)
         {
-            var totalMilliseconds = (long)delay.TotalMilliseconds;
-            await ManualDelay((int)totalMilliseconds, cancellationToken);
+            var totalMilliseconds = (long) delay.TotalMilliseconds;
+            await ManualDelay((int) totalMilliseconds, cancellationToken);
         }
 
         /// <summary>
-        /// Creates a cancellable task that completes after a specifiendnumber of milliseconds.
-        /// Task can be canceled in any time by calling <see cref="TryReleaseDelay()"/>
+        ///     Creates a cancellable task that completes after a specifiendnumber of milliseconds.
+        ///     Task can be canceled in any time by calling <see cref="TryReleaseDelay()" />
         /// </summary>
-        /// <param name="millisecondsDelay">The number of millisecond to wait before completing the return task, or -1 to wait indefinitely</param>
+        /// <param name="millisecondsDelay">
+        ///     The number of millisecond to wait before completing the return task, or -1 to wait
+        ///     indefinitely
+        /// </param>
         /// <returns>A task that represents the time delay.</returns>
         protected async Task ManualDelay(int millisecondsDelay)
         {
@@ -75,10 +79,13 @@ namespace Core
         }
 
         /// <summary>
-        /// Creates a cancellable task that completes after a specifiendnumber of milliseconds.
-        /// Task can be canceled in any time by calling <see cref="TryReleaseDelay()"/>
+        ///     Creates a cancellable task that completes after a specifiendnumber of milliseconds.
+        ///     Task can be canceled in any time by calling <see cref="TryReleaseDelay()" />
         /// </summary>
-        /// <param name="millisecondsDelay">The number of millisecond to wait before completing the return task, or -1 to wait indefinitely</param>
+        /// <param name="millisecondsDelay">
+        ///     The number of millisecond to wait before completing the return task, or -1 to wait
+        ///     indefinitely
+        /// </param>
         /// <param name="cancellationToken">A cancellation token to observe while waiting for the task to complete.</param>
         /// <returns>A task that represents the time delay.</returns>
         protected async Task ManualDelay(int millisecondsDelay, CancellationToken cancellationToken)
@@ -94,12 +101,19 @@ namespace Core
                 _logger.LogTrace(ex, $"{nameof(ManualDelay)}: manual delay is revoked.");
             }
 
-            _cancelTokenSource = new CancellationTokenSource();
-            _token = _cancelTokenSource.Token;
+            (_cancelTokenSource, _token) = CreateToken();
+        }
+
+        private (CancellationTokenSource, CancellationToken) CreateToken()
+        {
+            var cancelTokenSource = new CancellationTokenSource();
+            var token = cancelTokenSource.Token;
+            token.Register(() => _logger.LogDebug($"{nameof(ManualDelay)}: manual delay is revoked."));
+            return (cancelTokenSource, token);
         }
 
         /// <summary>
-        /// Release all resources used by the current instance of the <see cref="CancellationTokenSource"/> class.
+        ///     Release all resources used by the current instance of the <see cref="CancellationTokenSource" /> class.
         /// </summary>
         public override void Dispose()
         {
